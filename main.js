52bb52e7a401d8a0a2f01132b089215d6fb078cc
Yelena Cox <yelenacox@gmail.com>
2022-10-07 10:11:07 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (const chunks of clay) {
+   const mug = `${chunks} coffee mug`
+   toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln)
